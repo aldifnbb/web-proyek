7b3f7c4066cce638c420d9f33f297d281e7f4b40
depedro-1 <depedrompane@gmail.com>
2024-11-17 16:41:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import Materi from './pages/Materi';
 import Pengumuman from './pages/Pengumuman';
 import Quiz from './pages/Quiz';
 import ForumDiskusi from './pages/ForumDiskusi';
+import Bab1 from "./pages/Bab1";
+import Bab2 from "./pages/Bab2";
+import Bab3 from "./pages/Bab3";
+import Bab4 from "./pages/Bab4";
+import Bab5 from "./pages/Bab5";
 
 function App() {
   return (
@@ -37,6 +42,12 @@ function MainContent() {
         <Route path="/pengumuman" element={<Pengumuman />} />
         <Route path="/forum-diskusi" element={<ForumDiskusi />} />
         <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route path="/materi" element={<Materi />} />
+        <Route path="/bab1" element={<Bab1 />} />
+        <Route path="/bab2" element={<Bab2 />} />
+        <Route path="/bab3" element={<Bab3 />} />
+        <Route path="/bab4" element={<Bab4 />} />
+        <Route path="/bab5" element={<Bab5 />} />
       </Routes>
     </div>
   );
