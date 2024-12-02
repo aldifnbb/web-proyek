830a938517b0334d03ffb86458096125f77ca786
depedro-1 <depedrompane@gmail.com>
2024-12-02 11:13:12 +0700

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,34 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Contact = () => {
   return (
     <div className="contact-page">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">EconoMinds</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-              <Link className="nav-link" to="/ekonomi-sejarah" data-aos="fade-down">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact" data-aos="fade-down">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <header className="contact-header">
         <h1 className="contact-title">Contact Us</h1>
         <p className="contact-subtitle">
@@ -60,7 +35,7 @@ const Contact = () => {
               <label htmlFor="message">Message</label>
               <textarea
                 id="message"
-                placeholder="Hi Ganteng....."
+                placeholder="Send Messages....."
                 rows="5"
               ></textarea>
             </div>
@@ -74,15 +49,15 @@ const Contact = () => {
         <div className="contact-info">
           <h2>Contact Information</h2>
           <p>Jl. Sisingamangaraja, Tapanuli Utara, Sumatera Utara</p>
-          <p>081265455903</p>
+          <p>082276423194</p>
           <p> aldinababan96@gmail.com</p>
           <p> immanuellumbantoruan18@gmail.com</p>
           <p> permaisimangunsong@gmail.com</p>
           <p> depedrompane@gmail.com</p>
           <div className="social-icons">
-            <a href="#" className="social-icon">LinkedIn</a>
-            <a href="#" className="social-icon">Twitter</a>
-            <a href="#" className="social-icon">Facebook</a>
+            <a href="https://www.linkedin.com/" className="social-icon">LinkedIn</a>
+            <a href="https://x.com/?lang=en-id" className="social-icon">Twitter</a>
+            <a href="https://www.facebook.com/" className="social-icon">Facebook</a>
           </div>
         </div>
       </div>
